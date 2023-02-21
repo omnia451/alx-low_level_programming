0ca0ee96e1fcb4484b478d449df69a65735bcbb3
omnia451 <omniasaad191@gmail.com>
2023-02-20 18:59:40 -0800

    d

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - a program that prints a line
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
