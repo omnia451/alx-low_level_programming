51c3c22aad122391e57de5d871a95117e59a78af
omnia451 <omniasaad191@gmail.com>
2023-02-21 19:43:57 -0800

    d

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,14 +2,16 @@
 
 
 /**
- * print_alphabet - makethe alphabet 
+ * print_alphabet - make the alphabet 
  * Return: void
  */
 
 void print_alphabet(void)
 
 {
+
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 	_putchar(c);
