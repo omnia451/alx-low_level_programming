5f97925834c6686da0c6aa8e6a230a721e0700c8
omnia451 <omniasaad191@gmail.com>
2023-03-02 17:56:34 -0800

    d

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - change all lowercase to uppercase
+ * @a: pointer
+ * Return: a
+ */
+char *string_toupper(char *a)
+{
+	int i;
+
+	i = 0;
+	while (a[i] != '\0')
+	{
+		if (a[i] >= 'a' && a[i] <= 'z')
+			a[i] = a[i] - 32;
+		i++;
+	}
+	return (a);
+}
